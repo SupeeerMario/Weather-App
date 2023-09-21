e900aa10e5a1cea76642d2e6a5c689aab8cbe77c
SupeeerMario <khaledshaaban926@gmail.com>
2023-09-21 04:31:32 +0300

    feat: SearchBar

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPI_options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4b2505117cmsh05cff2700830569p15436ajsne36cb6a0c851',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const geoAPI_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
